d74674e2937b914813fd1cd0cedf93dde72c0918
Gabe Venberg <gabevenberg@gmail.com>
2026-06-16 11:36:26 +0000

    highlight QUESTION and WHY comments.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -62,6 +62,8 @@ require("mini.hipatterns").setup({
     hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
     todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
     note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+    question = { pattern = "%f[%w]()QUESTION()%f[%W]", group = "MiniHipatternsNote" },
+    why = { pattern = "%f[%w]()WHY()%f[%W]", group = "MiniHipatternsNote" },
 
     -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
